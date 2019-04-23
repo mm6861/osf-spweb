a220ed4403266a8afc67029ce693035109cc4e9a
mm6861 <woohoomi@nate.com>
2019-04-23 17:53:06 +0900

    4-23

--- src/main/java/com/osf/web/controller/HomeController.java
+++ src/main/java/com/osf/web/controller/HomeController.java
@@ -1,19 +1,42 @@
 package com.osf.web.controller;
 
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.extern.slf4j.Slf4j;
 @Controller
+@Slf4j
 public class HomeController {
+	@Autowired
+	private DataSource ds;
 	@RequestMapping("/")
 	public String goHome() {
+		try {
+			log.info("con=>{}",ds.getConnection());
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 		return "index";
 	}
 	@RequestMapping("/test")
 	public String goTest(Model model) {
+		try {
+			log.info("con=>{}",ds.getConnection());
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		model.addAttribute("msg","hello!");
 		List<String> strList = new ArrayList<String>();
 		strList.add("1");
@@ -21,6 +44,5 @@ public class HomeController {
 		strList.add("3");
 		model.addAttribute("list",strList);
 		return "test";
-	}
-	
+	}	
 }
